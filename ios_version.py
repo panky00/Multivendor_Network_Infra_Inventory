127f25a1dd03a479e7065fc9a68cd91ae56d38d9
panky00 <pankajsharma.blue@gmail.com>
2017-12-23 19:11:38 +0100

    Add files via upload

---
+++ ios_version.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+from netmiko import ConnectHandler
+from datetime import datetime
+
+import os
+import sys
+import time
+import select
+import paramiko
+import re
+datestring = datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M')
+os.chdir(r'C:\DEV\RAW_DATA')
+fd = open(r'cisco-ios-version.txt','w') 
+old_stdout = sys.stdout   
+sys.stdout = fd 
+platform = 'cisco_ios'
+username = 'pankaj.sharma'
+password = '1Ofnc8qOyb8WMzTIdm0ey5kp'
+
+
+ip_add_file = open(r'C:\DEV\INVENTORY\cisco7600.txt','r') 
+
+for host in ip_add_file:
+    host = host.strip()
+    device = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
+    output = device.send_command('terminal length 0')
+           
+    output = device.send_command('show version')
+    print(output)
+
+
+fd.close()
